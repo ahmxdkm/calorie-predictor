5fffa6a7021e49802b911a1a4a06e2c9ee24fa74
ahmxdkm <www.ahmedkm10@gmail.com>
2025-06-26 12:29:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load("calorie.sav")
+model = joblib.load("project.sav")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
